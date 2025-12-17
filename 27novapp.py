801166ce1dc274134477e2ae8b66995d18f519e7
aasthagit2025 <aastha.batra@knowledgeexcel.com>
2025-12-17 17:12:28 +0530

    Add files via upload

--- 27novapp.py
+++ 27novapp.py
@@ -605,6 +605,7 @@ def configure_mq_rules(all_variable_options):
                     else:
                         st.warning("Please select columns for the MQ group.")
 
+def generate_string_spss_syntax(rule):
     """
     Generates detailed SPSS syntax for a String check.
     """
@@ -702,7 +703,7 @@ def configure_string_rules(all_variable_options):
                 new_string_rules.append({
                     'variable': col,
                     'min_length': min_length,
-                    'run_skip': run_skip,
+                    'run_skip': run_skip and skip_trigger_col != '-- Select Variable --',
                     'trigger_col': skip_trigger_col,
                     'trigger_val': skip_trigger_val,
                 })
