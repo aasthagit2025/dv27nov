33ef4bcaae0b089f8c670300fbcba316909611c7
aasthagit2025 <aastha.batra@knowledgeexcel.com>
2026-01-06 18:18:56 +0530

    Add files via upload

--- 27novapp.py
+++ 27novapp.py
@@ -1078,7 +1078,7 @@ if uploaded_file:
         # ✅ CALL VARIABLE DETECTION HERE (AND ONLY HERE)
         if "var_types" not in st.session_state: st.session_state.var_types = detect_variable_types(df_raw) 
         
-        st.session_state.var_sq = st.session_state.var_typess["sq"]    
+        st.session_state.var_sq = st.session_state.var_types["sq"]    
         st.session_state.var_mq = st.session_state.var_types["mq"]
         st.session_state.var_oe = st.session_state.var_types["oe"]
         st.session_state.var_ranking = st.session_state.var_types["ranking"]
